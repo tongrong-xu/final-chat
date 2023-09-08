1ecec4a2a65ab719afad11524a0b1bb3cd857ad2
otohasan <love5252030@gmail.com>
2023-09-09 00:45:41 +0800

    all

--- app.js
+++ app.js
@@ -5,14 +5,19 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const http = require('http');
 const socketIOServer = require('socket.io');
-const { v4: uuidv4 } = require('uuid');
+const {
+  v4: uuidv4
+} = require('uuid');
 
 const path = require('path');
 
 const UserRoute = require('./routes/UserRoute');
 const socket = require('./socket.js');
 
-mongoose.connect("mongodb://127.0.0.1:27017/test");
+mongoose
+  .connect(process.env.DBServerUrl)
+  .then(x => console.log(`Connected the Database: "${x.connections[0].name}"`))
+  .catch(err => console.error('Error connecting to mongo', err));
 const {
   sessionSecret
 } = process.env;
@@ -34,8 +39,14 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(
+  cors({
+    origin: ["http://localhost:3000", process.env.ORIGIN],
+  })
+);
+
 app.set('socketio', io);
 app.use('/', UserRoute);
-server.listen(8080, () => console.log("Server started on port"));
+server.listen(3000, () => console.log("Server started on port"));
 
 socket.socketOn(io);
